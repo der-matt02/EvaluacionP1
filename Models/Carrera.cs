14b8f038a26cfdd829e61b8ea7d0f3dca494eedd
der-matt02 <dermatt02@gmail.com>
2024-05-02 07:44:05 -0500

    Declaración de las dos clases

---
+++ Models/Carrera.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EvaluacionP1.Models
+{
+    public class Carrera
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string id { get; set; }
+
+        public string nombreCarrera { get; set; }
+
+        public string campus { get; set; }
+        public int numeroSemestres { get; set; }
+
+        public Carrera(string id, int nombreCarrera, string campus, int numeroSemestres)
+        {
+            nombreCarrera = nombreCarrera;
+            campus = campus;
+            numeroSemestres = numeroSemestres;
+        }
+
+
+    }
+}
---
+++ Models/MBaquero.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EvaluacionP1.Models
+{
+    public class MBaquero
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string id { get; set; }
+        [Required] public string name { get; set; } = string.Empty;
+        public int edad { get; set; }
+        [AllowNull]
+        public decimal mensualidad { get; set; }
+        public DateTime anio { get; set; }
+        public bool reprobo { get; set; }
+
+        public MBaquero(string id, string nombre, int edad, decimal mensualidad, DateTime anio, bool reprobo)
+        {
+            nombre = nombre;
+            edad = edad;
+            mensualidad = mensualidad;
+            anio = anio;
+            reprobo = reprobo;
+        }
+
+        [ForeignKey("CarreraId")]
+        [AllowNull]
+        public int CarreraId { get; set; }
+        public Carrera? Carrera {get; set; }
+    }
+}
